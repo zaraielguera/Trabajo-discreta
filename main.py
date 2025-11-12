d2f2e83d80e96c3337c9b05e0fefc46c7faa77ca
zaraielguera <60016256@continental.edu.pe>
2025-11-12 13:11:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def saludo(nombres: str) -> str:
-    return f"Hola,{nombres}! Bienvenido a Git, commit exitoso"
+    return f"Hola,{nombres}! GENIAL...Bienvenido a Git, commit exitoso"
 
 if __name__ == "__main__":
     nombres= input(" Tu nombre?")
